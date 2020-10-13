89a98298e4deda31624ea0bd33a7a74954bddfc5
zonky2 <info@e-spin.de>
2020-10-13 12:10:04 +0200

    Fix plugin

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -21,7 +21,7 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Espin\FormRegexBundle\EspinPageInfoBundle;
+use Espin\PageInfoBundle\EspinPageInfoBundle;
 
 /**
  * @internal
