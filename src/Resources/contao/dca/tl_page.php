3755128c97cbad80d140e96dfb5947c4eb19c837
Ingolf Steinhardt <info@e-spin.de>
2026-02-18 18:28:48 +0100

    Add Support for Contao 5.7

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -3,12 +3,12 @@
 /**
  * This file is part of e-spin/page-info-bundle.
  *
- * Copyright (c) 2020-2024 e-spin
+ * Copyright (c) 2020-2026 e-spin
  *
  * @package   e-spin/page-info-bundle
  * @author    Ingolf Steinhardt <info@e-spin.de>
  * @author    Kamil Kuzminski <kamil.kuzminski@codefog.pl>
- * @copyright 2020-2024 e-spin
+ * @copyright 2020-2026 e-spin
  * @license   LGPL-3.0-or-later
  */
 
@@ -17,4 +17,5 @@ declare(strict_types=1);
 /**
  * Add info to tl_page panel.
  */
-$GLOBALS['TL_DCA']['tl_page']['list']['sorting']['panelLayout'] .= ',page_info';
+$GLOBALS['TL_DCA']['tl_page']['list']['sorting']['panelLayout'] =
+    'page_info,' . $GLOBALS['TL_DCA']['tl_page']['list']['sorting']['panelLayout'];
